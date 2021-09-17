b548ddddff786910da7086ad67947aea5c1b5151
Juan Fernando Osorno G <juanfer28@gmail.com>
2021-09-16 21:19:56 -0500

    Conexion y test de base de datos

---
+++ src/test/java/tests/TestGeneral.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import persistencia.ConexionBD;
+
+/**
+ *
+ * @author Cielo Juliana
+ */
+public class TestGeneral {
+    
+    @Test
+    public void verificarConexion() {
+        ConexionBD con = new ConexionBD();
+        Assert.assertTrue(con.isConectado(), "No hay conexión creada. ");
+        con.desconectar();
+        Assert.assertFalse(con.isConectado(), "No se cerró conexión. ");
+    }
+}
